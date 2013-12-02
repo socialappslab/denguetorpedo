cf355228203d5336495bd3ce87fe2c8ddaac8e8c
Hara Kang <hara0115@gmail.com>
2013-12-01 17:14:23 -0800

    Added some request specs

--- spec/requests/houses_spec.rb
+++ spec/requests/houses_spec.rb
@@ -1,15 +1,10 @@
 require 'spec_helper'
 
-describe "Reports" do
-  describe "GET /reports" do
-  	before(:each) do
-  		@user = FactoryGirl.create(:user)
-  		post session_path, email: @user.email, password: 'denguewarrior'
-  	end
+describe "Houses" do
+  describe "GET /houses" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      
-      get reports_path
+      get houses_path
       response.status.should be(200)
     end
   end
