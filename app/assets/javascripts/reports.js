28ea0f8248e55a396f33d1b89871453474115137
Andrew Dorsett <awdorsett@berkeley.edu>
2014-02-27 22:52:20 -0800

    new report form designed

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -26,6 +26,8 @@ angular.module('dengue_torpedo.controllers',['ngResource', 'timer']).
         controller('ReportListController',function($scope,$resource){
             allowed_time = 1000 * 60 * 60 * 48;  // milliseconds * sec * min * hour;
 
+            $scope.new_report = false;
+
             $scope.reports = {};
             $scope.date = new Date();//.getTime();
 
@@ -34,20 +36,24 @@ angular.module('dengue_torpedo.controllers',['ngResource', 'timer']).
                 console.log($scope.reports);
             });
 
-        $scope.time_left =function(report){
-            var start_time = new Date(report.info.completed_at);
-            return start_time.getTime() + allowed_time;
-        }
-
-        $scope.report_expired = function(report){
-            if(report.info.completed_at){
-                //report is expired if start_time + allowed_time < current_time
-                var start_time = new Date(report.info.completed_at);
-                return new Date(start_time.getTime() + allowed_time) < new Date().getTime();
-
+            $scope.create_new_report = function(){
+                $("#new_report").slideDown();
             }
 
-        }
+            $scope.time_left =function(report){
+                var start_time = new Date(report.info.completed_at);
+                return start_time.getTime() + allowed_time;
+            }
+
+            $scope.report_expired = function(report){
+                if(report.info.completed_at){
+                    //report is expired if start_time + allowed_time < current_time
+                    var start_time = new Date(report.info.completed_at);
+                    return new Date(start_time.getTime() + allowed_time) < new Date().getTime();
+
+                }
+
+            }
 
 
         });
