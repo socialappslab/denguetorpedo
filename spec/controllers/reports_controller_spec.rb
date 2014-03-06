2e126e30d4cf006aefe39cee58a5a89eb7610cba
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-05 23:37:46 -0800

    Added first few specs for ReportsController.
    * first spec for ReportsController#index
    * changed to use let block
    * check that only logged in users can create reports.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,14 +1,50 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe ReportsController do
-	before(:each) do
-		controller.stub(:require_login).and_return(true)
-		@report = FactoryGirl.create(:report)
+	#-----------------------------------------------------------------------------
+
+	describe "Getting a list of reports" do
+		it "return a list of reports" do
+			get :index
+		end
 	end
-	describe "DELETE /reports" do
+
+	#-----------------------------------------------------------------------------
+
+	describe "Creating a new report" do
+		let!(:user) { FactoryGirl.create(:user) }
+
+		before(:each) do
+			cookies[:auth_token] = user.auth_token
+		end
+
+		it "is only allowed for logged in users" do
+			cookies[:auth_token] = nil
+			post :create
+			expect(flash[:alert]).to eq("Faça o seu login para visualizar essa página.")
+		end
+
+		it "returns an error if no x or y coordinates are given" do
+			post :create
+			expect(flash[:alert]).to eq("Você precisa marcar uma localização válida para o seu foco.")
+		end
+
+	end
+
+	#-----------------------------------------------------------------------------
+
+	describe "Deleting reports" do
+		let(:report) { FactoryGirl.create(:report) }
+
 		it "should deduct points" do
-			delete :destroy, id: @report.id
+			delete :destroy, id: report.id
 			response.should be_redirect
 		end
 	end
+
+	#-----------------------------------------------------------------------------
+
+
 end
