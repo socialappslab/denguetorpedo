7de92e4391a536570de255e0fcfc77a054d237ad
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-05 23:37:46 -0800

    Added first few specs for ReportsController.
    * first spec for ReportsController#index
    * changed to use let block
    * check that only logged in users can create reports.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -2,33 +2,46 @@
 require 'spec_helper'
 
 describe ReportsController do
-	# before(:each) do
-	# 	controller.stub(:require_login).and_return(true)
-	# 	@report = FactoryGirl.create(:report)
-	# end
-	# describe "DELETE /reports" do
-	# 	it "should deduct points" do
-	# 		delete :destroy, id: @report.id
-	# 		response.should be_redirect
-	# 	end
-	# end
-
 	#-----------------------------------------------------------------------------
 
-	context "Creating a new report" do
-		render_views
-
-		describe "with errors" do
-			it "alerts user if description is not present" do
-			end
-		end
-
-		describe "successfully" do
-			it "creates a report if no map coordinates are present" do
-			end
+	describe "Getting a list of reports" do
+		it "return a list of reports" do
+			get :index
 		end
 	end
 
 	#-----------------------------------------------------------------------------
 
+	describe "Creating a new report" do
+		let!(:user) { FactoryGirl.create(:user) }
+
+		before(:each) do
+			cookies[:auth_token] = user.auth_token
+		end
+
+		it "is only allowed for logged in users" do
+			cookies[:auth_token] = nil
+			post :create
+			expect(flash[:alert]).to eq("Faça o seu login para visualizar essa página.")
+		end
+
+		it "returns an error if no x or y coordinates are given" do
+			post :create
+			expect(flash[:alert]).to eq("Você precisa marcar uma localização válida para o seu foco.")
+		end
+
+	end
+
+	#-----------------------------------------------------------------------------
+
+	describe "Deleting reports" do
+		let(:report) { FactoryGirl.create(:report) }
+
+		it "should deduct points" do
+			delete :destroy, id: report.id
+			response.should be_redirect
+		end
+	end
+
+	#-----------------------------------------------------------------------------
 end
