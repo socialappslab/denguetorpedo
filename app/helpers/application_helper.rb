96b9bd112f7f3ef4bccb42b44e935db525f4012f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-06 21:47:26 -0800

    Added login helper to ApplicationsHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
-  
+
   def self.temp_password_generator
     char_bank = ('0'..'9').to_a
     char_bank.shuffle.shuffle.shuffle!
     (1..8).collect{|a| char_bank[rand(char_bank.size)] }.join
   end
-    
+
+  def login_user(user)
+    return nil if user.nil?
+    cookies[:auth_token] = user.auth_token
+  end
+
   def highlight_active_link(path)
     "current_link" if current_page?(path)
   end
