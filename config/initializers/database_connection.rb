891cd0904fc926f41d2c41378d39f826ecaffd2c
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-12 15:58:04 -0700

    Make changes to database pool using Heroku config vars.

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -1,18 +1,10 @@
-Rails.application.config.after_initialize do
-  ActiveRecord::Base.connection_pool.disconnect!
-
-  #
-  ActiveSupport.on_load(:active_record) do
-
-    # The pool configuration is set to 20 *per process*
-    # because that's what the basic plan allows on Heroku.
-    # As long as we run Puma and Sidekiq in 2 separate processes,
-    # we should be fine setting thread-level concurrency to at
-    # most 20.
-    puts "Rails.env: #{Rails.env}"
-    config = ActiveRecord::Base.configurations[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool']              = 20
-    ActiveRecord::Base.establish_connection(config)
-  end
+# The pool configuration is set to 20 *per process*
+# because that's what the basic plan allows on Heroku.
+# As long as we run Puma and Sidekiq in 2 separate processes,
+# we should be fine setting thread-level concurrency to at
+# most 20.
+database_url = ENV['DATABASE_URL']
+if(database_url)
+  ENV['DATABASE_URL'] = "#{database_url}?pool=20"
+  ActiveRecord::Base.establish_connection
 end
