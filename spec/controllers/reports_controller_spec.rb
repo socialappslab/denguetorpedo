2cc2aef64ba2cea62140e9ad710c16430cf777fb
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-12 17:11:04 -0700

    Added spec for reports_controller (testing map coordinates).

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -2,17 +2,6 @@
 require 'spec_helper'
 
 describe ReportsController do
-	# before(:each) do
-	# 	controller.stub(:require_login).and_return(true)
-	# 	@report = FactoryGirl.create(:report)
-	# end
-	# describe "DELETE /reports" do
-	# 	it "should deduct points" do
-	# 		delete :destroy, id: @report.id
-	# 		response.should be_redirect
-	# 	end
-	# end
-
 	#-----------------------------------------------------------------------------
 
 	context "Creating a new report" do
@@ -24,7 +13,21 @@ describe ReportsController do
 		end
 
 		describe "successfully" do
+			let(:location) { FactoryGirl.create(:location) }
+
 			it "creates a report if no map coordinates are present" do
+			end
+
+			it "finds map coordinates even if user didn't submit them" do
+				expect(location.latitude).to  eq(nil)
+				expect(location.longitude).to eq(nil)
+
+				post :create, :street_type => location.street_type, :street_name => location.street_name, :street_number => location.street_number,
+				:report => { :report => "", :before_photo => "Test" }
+
+				location.reload
+				expect(location.latitude).to  eq(680291.2151545063)
+				expect(location.longitude).to eq(7471401.29586681)
 			end
 		end
 	end
