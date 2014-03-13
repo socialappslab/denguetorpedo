ebf768a3deef5f3deb96295e176ec2a6ae7c1104
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-12 17:02:39 -0700

    Search for coordinates on the location instance.

--- app/workers/map_coordinates_worker.rb
+++ app/workers/map_coordinates_worker.rb
@@ -3,12 +3,11 @@ class MapCoordinatesWorker
 
   sidekiq_options :queue => :map_coordinates, :retry => true, :backtrace => true
 
-  def perform(report_id)
-    # An exception will be raised if either instance hasn't been persisted yet.
-    report   = Report.find(report_id)
-    location = Location.find(report.location_id)
+  def perform(location_id)
+    # An exception will be raised if the instance hasn't been persisted yet.
+    location = Location.find(location_id)
 
-    # If the report's location already has map coordinates, then let's go ahead and exit.
+    # If the location already has map coordinates, then let's go ahead and exit.
     return if location.latitude.present? && location.longitude.present?
 
     # At this point, we know that both lat/long are missing so we need to fetch
@@ -19,7 +18,7 @@ class MapCoordinatesWorker
     # If the response returns no candidates, then let's raise an error so
     # that Sidekiq retries again.
     response = JSON.parse(Net::HTTP.get(parsed_server_url))
-    raise "No candidate found for location with id: #{report.location.id} and address: #{report.location.address}" if response["candidates"].blank?
+    raise "No candidate found for location with id: #{location.id} and address: #{location.address}" if response["candidates"].blank?
 
     # Return the most likely candidate and update the Location instance.
     candidate = response["candidates"].first
--- spec/workers/map_coordinates_worker_spec.rb
+++ spec/workers/map_coordinates_worker_spec.rb
@@ -2,13 +2,12 @@ require "spec_helper"
 
 describe MapCoordinatesWorker do
   let(:location) { FactoryGirl.create(:location) }
-  let(:report)   { FactoryGirl.create(:report, :location => location) }
 
   it "fetches map coordinates for a valid address" do
     expect(location.latitude).to eq(nil)
     expect(location.longitude).to eq(nil)
 
-    MapCoordinatesWorker.perform_async(report.id)
+    MapCoordinatesWorker.perform_async(location.id)
 
     location.reload
     expect(location.latitude).to  eq(680291.2151545063)
