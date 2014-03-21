4c17c66b9cc7b5bb964ce0065b675656b2e18d77
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-20 22:44:38 -0700

    Refactor homepage to include logged-in users
    * display news to logged-in users
    * display houses to logged-in users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,19 @@ class HomeController < ApplicationController
   def index
     @user = @current_user || User.new
 
-    @all_neighborhoods     = Neighborhood.order(:id).limit(3)
+    # NOTE: This is a hack that allows us to reuse this action with both
+    # logged-in and visitors. Basically, a logged-in user cares only about
+    # his/her own neighborhood.
+    if @current_user.present?
+      @all_neighborhoods = [ @current_user.neighborhood ]
+    else
+      @all_neighborhoods     = Neighborhood.order(:id).limit(3)
+    end
+
     @selected_neighborhood = @all_neighborhoods.first
     @participants          = @selected_neighborhood.members.where('role != ?', "lojista")
 
+    @notices      = @selected_neighborhood.notices.limit(5).order(:date)
     @houses       = @participants.map { |participant| participant.house }.uniq.shuffle
     @prizes       = Prize.where('stock > 0 AND (expire_on IS NULL OR expire_on > ?)', Time.new)
   end
