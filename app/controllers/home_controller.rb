d9f5f75edd441a157002fbff20bbb6a7878ac7d0
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-20 21:56:53 -0700

    Change search bar to dropdown
    Also add redirect ability upon choice.
    
    Conflicts:
    	app/controllers/home_controller.rb
    	app/views/home/index.html.haml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  #----------------------------------------------------------------------------
+  # GET /
+
   def index
     @user = @current_user || User.new
 
@@ -10,7 +13,23 @@ class HomeController < ApplicationController
     @prizes       = Prize.where('stock > 0 AND (expire_on IS NULL OR expire_on > ?)', Time.new)
   end
 
+  #----------------------------------------------------------------------------
+  # GET /howto
+
   def howto
     @sections = DocumentationSection.order("order_id ASC")
   end
+
+  #----------------------------------------------------------------------------
+  # POST /neighborhood-search
+  #
+  # Parameters:
+  # { "neighborhood"=>{"name"=>"Vila Autódromo"} }
+
+  def neighborhood_search
+    neighborhood = Neighborhood.find_by_name(params[:neighborhood][:name])
+    redirect_to neighborhood_path(neighborhood)
+  end
+
+  #----------------------------------------------------------------------------
 end
