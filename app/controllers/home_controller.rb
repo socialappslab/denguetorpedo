004de29703a33b24402edfd0b5926b2d6460dffc
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-03-20 21:33:21 -0700

    Clean up HomeController#index.
    Conflicts:
    	app/controllers/home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,42 +1,16 @@
 class HomeController < ApplicationController
   def index
-    @collapse = 'collapse'
+    @user = @current_user || User.new
 
-    if (flash[:user].nil?)
-      @user = User.new
-    else
-      @user = flash[:user]
-    end
+    @all_neighborhoods     = Neighborhood.order(:id).limit(3)
+    @selected_neighborhood = @all_neighborhoods.first
+    @participants          = @selected_neighborhood.members.where('role != ?', "lojista")
 
-    @all_neighborhoods = Neighborhood.order(:id).limit(3)
-
-    if params[:neighborhood].nil?
-      if @all_neighborhoods.first.nil?
-        @selected_neighborhood = Neighborhood.new
-      else
-        @selected_neighborhood = @all_neighborhoods.first
-      end
-    else
-      @selected_neighborhood = Neighborhood.find(params[:neighborhood])
-    end
-    @participants = @selected_neighborhood.members.where('role != ?', "lojista")
-
-    @houses = @participants.map { |participant| participant.house }.uniq.shuffle
-
-
-    @prizes = Prize.where('stock > 0 AND (expire_on IS NULL OR expire_on > ?)', Time.new)
-    @notices = @selected_neighborhood.notices.where('date > ?', Time.now).order(:date)[0..5]
-    @total_reports_in_neighborhood = @selected_neighborhood.total_reports.count
-    @opened_reports_in_neighborhood = @selected_neighborhood.open_reports.count
-    @eliminated_reports_in_neighborhood = @selected_neighborhood.eliminated_reports.count
-  end
-  respond_to do |format|
-    format.html
-    format.json { render json: { user: @user }}
+    @houses       = @participants.map { |participant| participant.house }.uniq.shuffle
+    @prizes       = Prize.where('stock > 0 AND (expire_on IS NULL OR expire_on > ?)', Time.new)
   end
 
   def howto
     @sections = DocumentationSection.order("order_id ASC")
   end
-
 end
