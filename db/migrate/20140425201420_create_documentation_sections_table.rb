a561bb375ace21c58070b72206ba95390a377ca2
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-04-25 13:19:34 -0700

    Migrate to add DocumentSections.

---
+++ db/migrate/20140425201420_create_documentation_sections_table.rb
@@ -0,0 +1,16 @@
+class CreateDocumentationSectionsTable < ActiveRecord::Migration
+  def up
+    create_table :documentation_sections do |t|
+      t.string :title
+      t.text   :content
+      t.integer :editor_id
+      t.integer :creator_id
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :documentation_sections
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140424191607) do
+ActiveRecord::Schema.define(:version => 20140425201420) do
 
   create_table "contacts", :force => true do |t|
     t.string   "title"
     t.string   "email"
     t.string   "name"
     t.text     "message"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "documentation_sections", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "editor_id"
+    t.integer  "creator_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
