39d34b054d451daa377b13b5e26785e8f4c9c8b6
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-04-29 23:06:23 -0700

    Provide a strategy to rollback status_cd.

--- db/migrate/20140425202505_remove_status_cd_from_reports.rb
+++ db/migrate/20140425202505_remove_status_cd_from_reports.rb
@@ -1,5 +1,9 @@
 class RemoveStatusCdFromReports < ActiveRecord::Migration
-  def change
+  def up
     remove_column :reports, :status_cd
+  end
+
+  def down
+    add_column :reports, :status_cd, :integer
   end
 end
--- db/migrate/20140425204643_change_status_to_string.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStatusToString < ActiveRecord::Migration
-  def change
-    # NOTE: We get the following error in
-    # staging:
-    # PG::UndefinedColumn: ERROR:  column "status" of relation "reports" does not exist
-    # remove_column :reports, :status
-    add_column :reports, :status, :string
-  end
-end
