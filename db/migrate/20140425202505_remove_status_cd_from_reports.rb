6acc7819caf71b5222bae8951691991eb71f6d1a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-04-29 23:18:43 -0700

    Add status column and seed. Do not remove status_cd column.

--- db/migrate/20140425202505_remove_status_cd_from_reports.rb
+++
@@ -1,9 +0,0 @@
-class RemoveStatusCdFromReports < ActiveRecord::Migration
-  def up
-    remove_column :reports, :status_cd
-  end
-
-  def down
-    add_column :reports, :status_cd, :integer
-  end
-end
