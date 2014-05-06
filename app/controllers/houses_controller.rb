c94952277ed4bc28797962d16a3d6d2cebdf060c
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-05-05 18:34:54 -0700

    Add new and create action of houses controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -24,4 +24,46 @@ class HousesController < NeighborhoodsBaseController
     @open_markers.compact!
     @eliminated_markers.compact!
   end
+
+  def new
+    @house = House.new
+    @house_name_confirmation = false
+  end
+
+  def create
+    @house = House.new(params[:house])
+
+    house = House.find_by_name(params[:house][:name])
+    if params[:house][:name_confirmation].blank? && house.present? && @current_user.house_id != house.id
+      @current_user.house = house
+
+      @house_name_confirmation = true
+      flash[:alert] = "Uma casa com esse nome já existe. Você quer se juntar a essa casa? Se sim, clique confirmar. Se não, clique cancelar e escolha outro nome de casa."
+
+      render "new" and return
+    end
+
+
+    if @house.save
+      flash[:notice] = "Casa atualizado com sucesso!"
+      redirect_to neighborhood_house_path(@neighborhood, @house) and return
+    else
+      render "new" and return
+    end
+
+
+  end
+
+  #----------------------------------------------------------------------------
+
+  private
+
+  #----------------------------------------------------------------------------
+
+  def redirect_if_missing_house
+    redirect_to new_neighborhood_house_path(@neighborhood) if @current_user.house.blank?
+  end
+
+  #----------------------------------------------------------------------------
+
 end
