1950fe3b3fe0aa341fe9918b5db4a7158258638d
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-05-07 11:28:23 -0700

    Add rake task to populate users.

---
+++ lib/tasks/db.rake
@@ -0,0 +1,12 @@
+require './spec/support/data_helper.rb'
+
+namespace :db do
+  namespace :development do
+    desc "Populate with dummy data"
+    task :prepare => [:environment] do
+      ['db:reset', 'db:create', 'db:schema:load', 'db:seed'].each { |s| Rake::Task[s].invoke }
+
+      populate_notices_houses_sponsors_and_prizes()
+    end
+  end
+end
--- spec/support/data_helper.rb
+++ spec/support/data_helper.rb
@@ -2,33 +2,22 @@
 
 #------------------------------------------------------------------------------
 
-def populate_notices
+def populate_notices_houses_sponsors_and_prizes
   mare = Neighborhood.find_by_name('Maré')
 
   10.times do |index|
     Notice.create!(:neighborhood_id => mare.id, :title => "Hello News ##{index}!", :description => "We are now live for the #{index}th time!")
   end
-end
-
-#------------------------------------------------------------------------------
-
-def populate_houses
-  mare = Neighborhood.find_by_name('Maré')
 
   5.times do |index|
     House.create!(:neighborhood_id => mare.id, :name => "House #{index}!")
   end
 
-  House.create!(:neighborhood_id => mare.id, :name => "House Sponsor!", :house_type => "lojista")
-end
-
-#------------------------------------------------------------------------------
-
-def populate_sponsors
-  mare = Neighborhood.find_by_name('Maré')
 
   5.times do |index|
-    User.create!(:email => "sponsor_#{index}@denguetorpedo.com", :neighborhood_id => mare.id, :role => User::Types::SPONSOR, :password => "abcdefg", :first_name => "Senor", :last_name => "Sponsor ##{index}")
+    h = House.create!(:neighborhood_id => mare.id, :name => "House Sponsor #{index}!", :house_type => User::Types::SPONSOR)
+    u = User.create!(:email => "sponsor_#{index}@denguetorpedo.com", :house_id => h.id, :neighborhood_id => mare.id, :role => User::Types::SPONSOR, :password => "abcdefg", :first_name => "Senor", :last_name => "Sponsor ##{index}")
+    Prize.create!(:user_id => u.id, :prize_name => "Prize ##{index}", :description => "This is a prize ##{index}", :cost => index * 100, :stock => index, :neighborhood_id => mare.id)
   end
 end
 
