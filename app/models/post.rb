26bd4ad613342238e9abba5fd33d8f15b3d08be7
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-05-20 15:56:35 -0700

    Don't require title on posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,20 +8,20 @@ class Post < ActiveRecord::Base
   belongs_to :wall, :polymorphic => true
 
   # validations
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :user_id, :presence => true
-  validates :content, :presence => true, :length => {:minimum => 1, :maximum => 350}
+  validates :content, :presence => true, :length => {:minimum => 1}
 
   after_create :give_points
   after_destroy :remove_points
   after_create do |post|
     Feed.create_from_object(post, post.user_id, :post)
   end
-  
+
   def strf_updated_at
     self.updated_at.strftime("%d/%m/%Y")
   end
-  
+
   def strf_created_at
     self.created_at.strftime("%d/%m/%Y")
   end
