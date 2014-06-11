ce2ea995d838f45a34419021d083a1e9fbceae27
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-11 10:55:24 -0700

    Add user-comment association.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,7 @@ class Comment < ActiveRecord::Base
   #----------------------------------------------------------------------------
 
   belongs_to :commentable, :polymorphic => true
+  belongs_to :user
 
   #----------------------------------------------------------------------------
 end
