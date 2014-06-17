88c968e4f1ed853e8eaf56026d197588eb6c762e
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-17 09:02:54 -0700

    Add coordinator/verifier blog posts to the news feed in community page.

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -8,8 +8,8 @@ class NeighborhoodsController < NeighborhoodsBaseController
 
     # Identify the different types of users in the community.
     @participants = @neighborhood.members.where('role != ?', User::Types::SPONSOR).where(is_blocked: false).order(:first_name)
-    @coordinators = @participants.where(:role => User::Types::COORDINATOR)
-    @verifiers    = @participants.where(:role => User::Types::VERIFIER)
+    # @coordinators = @participants.where(:role => User::Types::COORDINATOR)
+    # @verifiers    = @participants.where(:role => User::Types::VERIFIER)
     @sponsors     = @neighborhood.members.where(:role => User::Types::SPONSOR).where(is_blocked: false)
 
     # Fetch all houses that have at least one member.
@@ -23,8 +23,16 @@ class NeighborhoodsController < NeighborhoodsBaseController
     @opened_reports_in_neighborhood     = @reports.where( :status => Report::STATUS[:reported] ).count
     @eliminated_reports_in_neighborhood = @reports.where( :status => Report::STATUS[:eliminated] ).count
 
-    @coordinator_blogs = @coordinators.map { |coor| coor.posts.last }.select{ |x| !x.nil?}.sort { |x, y| y.created_at <=> x.created_at}
-    @verifier_blogs    = @verifiers.map    { |veri| veri.posts.last }.select{ |x| !x.nil?}.sort { |x, y| y.created_at <=> x.created_at }
+    @posts = []
+    community_coordinators = @participants.where("role = ? OR role = ?", User::Types::COORDINATOR, User::Types::VERIFIER)
+    community_coordinators.each do |cc|
+      @posts << cc.posts
+    end
+    @posts.flatten!
+
+    @news_feed = (@posts.to_a + @notices.to_a).sort{|a,b| b.created_at <=> a.created_at }
+    # @coordinator_blogs = @coordinators.map { |coor| coor.posts.last }.select{ |x| !x.nil?}.sort { |x, y| y.created_at <=> x.created_at}
+    # @verifier_blogs    = @verifiers.map    { |veri| veri.posts.last }.select{ |x| !x.nil?}.sort { |x, y| y.created_at <=> x.created_at }
   end
 
   #----------------------------------------------------------------------------
