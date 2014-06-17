dce236b764c50131ba82454a96d876bfda4beeda
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-17 12:53:39 -0700

    Remove pretty print and format focos marcados content box.

--- app/helpers/neighborhoods_helper.rb
+++ app/helpers/neighborhoods_helper.rb
@@ -1,11 +1,2 @@
 module NeighborhoodsHelper
-  include ActionView::Helpers::NumberHelper
-
-  #----------------------------------------------------------------------------
-
-  def pretty_print_points(points)
-    number_to_human(points, precision: 3, :units => {:thousand => "K", :million => "M", :billion => "B"}, :separator => ".", :format => "%n%u")
-  end
-
-  #----------------------------------------------------------------------------
 end
