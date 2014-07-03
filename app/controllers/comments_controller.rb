4700a7adcc566db5611814e0a8da247e51841c13
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-07-02 21:46:57 -0700

    Remove unecessary comments_controller stuff.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,16 @@
 # encoding: UTF-8
 
-class CommentssController < ApplicationController
+class CommentsController < ApplicationController
   before_filter :require_login
-  before_filter :identify_comment
-  before_filter :ensure_valid_user
-
-  #----------------------------------------------------------------------------
 
   def destroy
-    if @comment.destroy
-      render :json => {:success => true} and return
-    else
-      render :json => {:success => false} and return
-    end
-  end
-
-  #----------------------------------------------------------------------------
-
-  private
-
-  #----------------------------------------------------------------------------
-
-  def identify_comment
     @comment = Comment.find(params[:id])
-  end
-
-  # This method verifies that the user fiddling with the comment is the one
-  # who created it.
-  def ensure_valid_user
-    if @comment.user_id != @current_user.id
-      render :json => { message: "You do not have permission to edit this comment" }, :status => 401
+    if @comment.destroy
+      flash[:notice] = I18n.t("views.comments.success_delete_flash")
+      redirect_to :back
+    else
+      flash[:alert] = I18n.t("views.application.error")
+      redirect_to :back
     end
   end
-
-  #----------------------------------------------------------------------------
-
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
   #----------------------------------------------------------------------------
 
   belongs_to :commentable, :polymorphic => true
-  belongs_to :user, :dependent => :destroy
+  belongs_to :user
 
   #----------------------------------------------------------------------------
 
