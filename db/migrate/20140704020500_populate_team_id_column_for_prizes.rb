129bdc0bfc7cb3ced2a3368d86fcfa096714060e
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-07-03 19:48:01 -0700

    Add migration file to transfer houses to teams for prizes.

---
+++ db/migrate/20140704020500_populate_team_id_column_for_prizes.rb
@@ -0,0 +1,21 @@
+class PopulateTeamIdColumnForPrizes < ActiveRecord::Migration
+  def up
+    Prize.find_each do |p|
+      house = p.user.house
+      if house.nil?
+        puts "[!] Couldn't find house for prize #{p.prize_name} (id: #{p.id})"
+        next
+      end
+
+      puts "[ok] Found house #{house.name} for prize #{p.prize_name} (id: #{p.id})..."
+      team  = Team.find_by_name(house.name)
+      puts "[ok] Found corresponding team #{team.name}..."
+
+      p.update_attribute(:team_id, team.id)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
