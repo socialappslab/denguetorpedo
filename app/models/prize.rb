306d5937990d3232b8c3a1748260120e6968c05f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-07-03 19:48:46 -0700

    Cleanup Prize model.

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -3,22 +3,24 @@
 class Prize < ActiveRecord::Base
   #----------------------------------------------------------------------------
 
-  attr_accessible :cost, :neighborhood_id, :description, :expire_on, :prize_name, :redemption_directions, :stock, :user_id, :prize_photo, :community_prize, :self_prize, :is_badge, :user, :prazo
-
-  #----------------------------------------------------------------------------
-
-  belongs_to :user
-  has_many :prize_codes, :dependent => :destroy
   has_attached_file :prize_photo, :default_url => 'home_images/logo.png', :styles => { :small => "60x60>", :large => "150x150>" }#, :storage => STORAGE, :s3_credentials => S3_CREDENTIALS
 
   #----------------------------------------------------------------------------
+  # Associations
+
+  belongs_to :user
+  belongs_to :neighborhood
+  belongs_to :team
+  has_many   :prize_codes, :dependent => :destroy
+
+  #----------------------------------------------------------------------------
+  # Validations
 
   validates :cost, :presence => true
   validates :description, :presence => true
   validates :prize_name, :presence => true
   validates :stock, :presence => true
   validates :user, :presence => true
-  belongs_to :neighborhood
 
   #----------------------------------------------------------------------------
 
