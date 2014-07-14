019302affd9b7102e3cb36fe5b8dee99a2a69777
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-07-14 14:33:03 -0700

    Add ensure_team_chosen for NeighborhoodBaseController.

--- app/controllers/neighborhoods_base_controller.rb
+++ app/controllers/neighborhoods_base_controller.rb
@@ -1,10 +1,20 @@
 class NeighborhoodsBaseController < ApplicationController
   before_filter :identify_neighborhood
+  before_filter :ensure_team_chosen
 
   #----------------------------------------------------------------------------
 
   def identify_neighborhood
     @neighborhood = Neighborhood.find_by_id(params[:neighborhood_id])
+  end
+
+  #----------------------------------------------------------------------------
+
+  def ensure_team_chosen
+    if @current_user && @current_user.teams.count == 0
+      flash[:notice] = I18n.t("views.teams.call_to_action_flash")
+      redirect_to teams_path and return
+    end
   end
 
   #----------------------------------------------------------------------------
