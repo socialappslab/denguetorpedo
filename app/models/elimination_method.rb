70ef870d97fe5ca3d44d20c6bd3aad056862760f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-08-01 11:05:30 -0700

    Update EliminationMethod model.

--- app/models/elimination_method.rb
+++ app/models/elimination_method.rb
@@ -1,7 +1,17 @@
 class EliminationMethod < ActiveRecord::Base
-  # attr_accessible :title, :body
   belongs_to :elimination_type
+  belongs_to :breeding_site
 
+  #----------------------------------------------------------------------------
 
-  
+  def description
+    if I18n.locale == :es
+      return self.description_in_es
+    else
+      return self.description_in_pt
+    end
+  end
+
+  #----------------------------------------------------------------------------
+
 end
