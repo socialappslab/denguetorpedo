7c4a3c144bcf8493758831a2d6d600e7f73614dd
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-08-01 10:58:30 -0700

    Update EliminationMethod with new columns.
    * Add breeding_site_id
    * Add description_in_pt
    * Add description_in_es.

---
+++ db/migrate/20140801174033_add_columns_to_elimination_methods.rb
@@ -0,0 +1,7 @@
+class AddColumnsToEliminationMethods < ActiveRecord::Migration
+  def change
+    add_column :elimination_methods, :breeding_site_id,  :integer
+    add_column :elimination_methods, :description_in_pt, :string
+    add_column :elimination_methods, :description_in_es, :string
+  end
+end
