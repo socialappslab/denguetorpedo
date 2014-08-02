02927cafa9d0d4e0b0a70602776e179c9c925752
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-08-02 13:49:21 -0700

    Skip useless EliminationMethod instances (no elimination type).

--- db/migrate/20140801175053_update_elimination_data.rb
+++ db/migrate/20140801175053_update_elimination_data.rb
@@ -13,6 +13,11 @@ class UpdateEliminationData < ActiveRecord::Migration
     EliminationMethod.order(:id).each do |em|
       puts "[ ] Updating EliminationMethod with id = #{em.id}..."
 
+      if em.elimination_type.blank?
+        puts "[!] EliminationMethod has no associated EliminationType!"
+        next
+      end
+
       # Identify the corresponding breeding site from elimination type.
       breeding_site = BreedingSite.find_by_description_in_pt( em.elimination_type.name )
       raise "BreedingSite could not be found for elimination type = #{em.elimination_type.name}!" if breeding_site.nil?
