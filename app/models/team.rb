c9948062dc3905c5890087450018a93fa3a2abd0
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-08-24 07:58:37 -0700

    Destroy all prizes if a team is destroyed.

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,7 @@ class Team < ActiveRecord::Base
 
   has_many :team_memberships, :dependent => :destroy
   has_many :users, :through => :team_memberships
+  has_many :prizes, :dependent => :destroy
 
   #----------------------------------------------------------------------------
 
