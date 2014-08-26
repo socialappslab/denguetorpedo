5b04fafea657989735b65b649fdbed8b7fbf5e3f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-08-26 10:30:32 -0700

    Add I18n locale to password reset email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,14 @@
 class UserMailer < ActionMailer::Base
   default from: "reportdengue@gmail.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
+  #----------------------------------------------------------------------------
+
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Resetar a senha"
+    mail to: user.email, subject: I18n.t("views.user_mailer.password_reset.subject")
   end
+
+  #----------------------------------------------------------------------------
 
   def group_buy_in_invitation(user, group)
     @user = user
@@ -17,11 +16,15 @@ class UserMailer < ActionMailer::Base
     mail to: user.email, subject: "Group Buy In Invitation!!!"
   end
 
+  #----------------------------------------------------------------------------
+
   def decline_invitation(user, buyIn)
     @user = user
     @buyIn = buyIn
     mail to: user.email, subject: "Friend has declined your Group By In Invitation"
   end
+
+  #----------------------------------------------------------------------------
 
   def accept_invitation(user, buyIn)
     @user = user
@@ -29,11 +32,15 @@ class UserMailer < ActionMailer::Base
     mail to: user.email, subject: "Friend has accepted your Group By In Invitation"
   end
 
+  #----------------------------------------------------------------------------
+
   def item_bought(user, group)
     @user = user
     @group = group
     mail to: user.email, subject: "Group Buy In Completed!!!"
   end
+
+  #----------------------------------------------------------------------------
 
   def send_contact(title, email, name, message)
     @title = title
@@ -42,5 +49,7 @@ class UserMailer < ActionMailer::Base
     @message = message
     mail from: @email, to: "denguetorpedo@gmail.com", subject: "[Contato DT] " + @title
   end
-  
+
+  #----------------------------------------------------------------------------
+
 end
