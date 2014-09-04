4e7a77669d77b71a8f8bd1c434d741c0540ad13f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-09-04 00:32:38 -0700

    Update validations to be validated in order, and allow blank emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,17 +29,19 @@ class User < ActiveRecord::Base
   # Validators
   #-----------
 
+  validates :username, :presence => true
+  validates :username, :uniqueness => true
+  validates :password, :length => { :minimum => 4}, :if => "id.nil? || password"
   validates :first_name, presence: true, :length => { :minimum => 2, :maximum => 16 }
   validates :last_name, presence: true, :length => { :minimum => 2, :maximum => 16 }
-  validates :password, :length => { :minimum => 4}, :if => "id.nil? || password"
+  validates :neighborhood_id, :presence => true
+
+  validates :email, :format => { :with => EMAIL_REGEX }, :allow_blank => true
+
   validates :points, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
   validates :total_points, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0}
 
-  validates :email, :format => { :with => EMAIL_REGEX }, :allow_nil => true
 
-  validates :neighborhood_id, :presence => true
-  validates :username, :presence => true
-  validates :username, :uniqueness => true
 
   #----------------------------------------------------------------------------
 
