8a0b1892cd65eb89ce302ff6d63cf0c931041a56
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-09-24 10:18:54 -0700

    Migrate to add messages table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140910174454) do
+ActiveRecord::Schema.define(:version => 20140924171657) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -156,6 +156,13 @@ ActiveRecord::Schema.define(:version => 20140910174454) do
     t.string   "street_type",       :default => ""
     t.string   "street_name",       :default => ""
     t.string   "street_number",     :default => ""
+  end
+
+  create_table "messages", :force => true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "neighborhoods", :force => true do |t|
