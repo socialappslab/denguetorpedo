44dad76c5bf2c56a271b137eb08436a338199fc5
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-10-02 12:11:37 -0700

    Allow login by username OR email.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,30 +9,27 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username( params[:username] )
 
+    # Try to identify the user by email if username fails. This is because we
+    # used to require login by email, but moved over to usernames.
+    user = User.find_by_email( params[:username] ) if user.nil?
+
     if user && user.authenticate(params[:password])
-
-      if user.is_blocked == false
-        if params[:remember_me]
-          cookies.permanent[:auth_token] = user.auth_token
-        else
-          cookies[:auth_token] = user.auth_token
-        end
-
-        respond_to do |format|
-          # NOTE: We're disabling showing a notice per conversation between
-          # @dman7 and @jamesholston
-          # "The folks in Brazil did not like the green banners saying logged in and logged out.
-          # It's not a question of Portuguese.  This is what banking and other commercial
-          # sites do and they did not want that association.
-          # So let's remove it for now until we have more time to look into the question.
-          # Can you just bracket it rather than delete it?"
-          format.html { redirect_to user_path(user) }
-          format.json { render json: {auth_token: user.auth_token}}
-        end
-      else
-        redirect_to root_url, :alert => I18n.t("views.application.user_blocked")
+      if user.is_blocked == true
+        redirect_to root_url, :alert => I18n.t("views.application.user_blocked") and return
       end
 
+      cookies.permanent[:auth_token] = user.auth_token
+      respond_to do |format|
+        # NOTE: We're disabling showing a notice per conversation between
+        # @dman7 and @jamesholston
+        # "The folks in Brazil did not like the green banners saying logged in and logged out.
+        # It's not a question of Portuguese.  This is what banking and other commercial
+        # sites do and they did not want that association.
+        # So let's remove it for now until we have more time to look into the question.
+        # Can you just bracket it rather than delete it?"
+        format.html { redirect_to user_path(user) }
+        format.json { render json: {auth_token: user.auth_token}}
+      end
     else
       redirect_to root_url, :alert => I18n.t("views.flashes.login.error")
     end
