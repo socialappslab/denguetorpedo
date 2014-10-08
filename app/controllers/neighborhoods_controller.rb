99a85e1b7c4fb40c5b1c46cb8b8284fdfab96578
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-10-07 22:55:23 -0700

    Optimize neighborhoods#show action.

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,33 +1,31 @@
 class NeighborhoodsController < NeighborhoodsBaseController
-  before_filter :ensure_team_chosen,    :only => [:show]
+  before_filter :ensure_team_chosen, :only => [:show]
+  before_filter :load_associations,  :only => [:show, :feed]
 
   #----------------------------------------------------------------------------
   # GET /neighborhoods/1
 
   def show
-    @neighborhood = Neighborhood.find(params[:id])
+    @post = Post.new
 
-    # Identify the users, and reports.
-    @participants = @neighborhood.members.includes(:posts).where(is_blocked: false).order("first_name ASC")
-    @reports      = @neighborhood.reports
-    @notices      = @neighborhood.notices.order("updated_at DESC")
+    # Limit the amount of records we show.
+    @reports = @reports.limit(5)
+    @notices = @notices.limit(5)
+    @posts   = @users.includes(:posts).map {|u| u.posts.limit(1)}.flatten
 
-    @teams = @neighborhood.teams.order("name ASC")
-    @teams = @teams.find_all { |t| t.users.count > 0 }
+    # Create the news feed.
+    @activity_feed = (@reports.to_a + @posts.to_a + @notices.to_a).sort{|a,b| b.created_at <=> a.created_at }
 
-    @total_reports_in_neighborhood      = @reports.count
-    @opened_reports_in_neighborhood     = @reports.find_all {|r| r.open? }.count
-    @eliminated_reports_in_neighborhood = @reports.find_all {|r| r.eliminated? }.count
+    @total_reports = @reports.count
+    @total_points  = @neighborhood.total_points
+  end
 
-    @total_points = @neighborhood.total_points
+  #----------------------------------------------------------------------------
 
-    @posts = []
-    @participants.each do |cc|
-      @posts << cc.posts
-    end
-    @posts.flatten!
-
-    @news_feed = (@reports.to_a + @posts.to_a + @notices.to_a).sort{|a,b| b.created_at <=> a.created_at }
+  def feed
+    @post          = Post.new
+    @posts         = @users.includes(:posts).map {|u| u.posts }.flatten
+    @activity_feed = (@reports.to_a + @posts.to_a + @notices.to_a).sort{|a,b| b.created_at <=> a.created_at }
   end
 
   #----------------------------------------------------------------------------
@@ -40,4 +38,18 @@ class NeighborhoodsController < NeighborhoodsBaseController
   end
 
   #----------------------------------------------------------------------------
+
+  private
+
+  def load_associations
+    @neighborhood = Neighborhood.find(params[:id]) if @neighborhood.nil?
+
+    @users   = @neighborhood.members.where(:is_blocked => false).order("first_name ASC")
+    @teams   = @neighborhood.teams.order("name ASC")
+    @reports = @neighborhood.reports
+    @notices = @neighborhood.notices
+  end
+
+  #----------------------------------------------------------------------------
+
 end
