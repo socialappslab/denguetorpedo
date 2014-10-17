01431a181f6c56488e23777befd164f3d483469a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-10-17 16:39:46 -0700

    Order neighborhoods by points in city#show.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -12,6 +12,10 @@ class CitiesController < ApplicationController
   def show
     @city          = City.find( params[:id] )
     @neighborhoods = Neighborhood.where(:city_id => @city.id)
+
+    # Calculate ranking for each community.
+    rankings  = @neighborhoods.map {|n| [n, n.total_points]}
+    @rankings = rankings.sort {|a, b| a[1] <=> b[1]}.reverse
   end
 
 
