7a1d4177ae6cdd9032bb4c750589239bfb213b5c
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-10-23 14:07:29 -0700

    Fix expiration time bug in reports page.
    Closes #329.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,22 +18,6 @@ module ApplicationHelper
 
   #----------------------------------------------------------------------------
 
-  # def elimination_selection(selection)
-  #   selection_list = []
-  #   if type = BreedingSite.find_by_description_in_pt(selection) || type = BreedingSite.find_by_description_in_es(selection)
-  #
-  #     type.elimination_methods.each do |method|
-  #       selection_list << {:name=> method.id, :points => method.points,:id => method.id,
-  #                          :display=> method.description + " (" + method.points.to_s + " pontos)"}
-  #     end
-  #
-  #   end
-  #
-  #   return selection_list
-  # end
-
-  #----------------------------------------------------------------------------
-
   def format_timestamp(timestamp)
     if (timestamp - Time.now).abs < 7.days
       time_ago_in_words(timestamp) + " " + I18n.t("common_terms.ago")
