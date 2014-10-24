63cfbe5ee0369c2b12566e6a526d1c5aaf770867
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-10-24 13:20:16 -0700

    Don't remove non-existing columns.

--- db/migrate/20141023221404_cleanup_tables.rb
+++ db/migrate/20141023221404_cleanup_tables.rb
@@ -1,12 +1,5 @@
 class CleanupTables < ActiveRecord::Migration
   def up
-    remove_column :reports, :nation
-    remove_column :reports, :state
-    remove_column :reports, :city
-    remove_column :reports, :address
-    remove_column :reports, :neighborhood
-    remove_column :reports, :status_cd
-    remove_column :reports, :feed_type_cd
     remove_column :reports, :elimination_type
     remove_column :reports, :elimination_method
     remove_column :reports, :reporter_name
@@ -18,13 +11,6 @@ class CleanupTables < ActiveRecord::Migration
   end
 
   def down
-    add_column :reports, :nation, :string
-    add_column :reports, :state, :string
-    add_column :reports, :city, :string
-    add_column :reports, :address, :string
-    add_column :reports, :neighborhood, :string
-    add_column :reports, :status_cd, :integer
-    add_column :reports, :feed_type_cd, :integer
     add_column :reports, :elimination_type, :string
     add_column :reports, :elimination_method, :string
     add_column :reports, :reporter_name, :string
