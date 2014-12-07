28da2d076e7abbb5cbfaa3051da8ac0dce387254
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-12-06 19:52:45 -0800

    Save device name and model when registering.

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -4,18 +4,20 @@ class API::V0::SessionsController < API::V0::BaseController
   #----------------------------------------------------------------------------
   # POST /api/v0/sessions
   def create
-    device   = params[:device]
+    device = params[:device]
 
     user = User.find_by_username( params[:username] )
     user = User.find_by_email( params[:username] ) if user.nil?
 
-    puts "user.inspect: #{user.inspect} | user.authenticate(params[:password]): #{user.authenticate(params[:password])}"
-
-    if user.present? && user.authenticate(params[:password]) && device
+    if user && user.authenticate(params[:password]) && device
       ds         = DeviceSession.new
       ds.user_id = user.id
       ds.token   = SecureRandom.uuid
+      ds.device_name  = device[:device]
+      ds.device_model = device[:model]
       ds.save!
+
+      puts "ds: #{ds.inspect}"
 
       render :json => { :device_session => { :token => ds.token } }, :status => 200
     else
