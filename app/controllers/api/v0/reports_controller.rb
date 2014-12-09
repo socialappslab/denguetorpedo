826031aa80a59687ef06814eccad7e8bcba11f7d
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-12-08 20:30:58 -0800

    Add API reports routes, and customize response body.

--- app/controllers/api/v0/reports_controller.rb
+++ app/controllers/api/v0/reports_controller.rb
@@ -3,7 +3,7 @@ class API::V0::ReportsController < API::V0::BaseController
   # GET /api/v0/reports
 
   def index
-    render :json => @user.reports, :status => 200 and return
+    render :json => {:reports => @user.reports.order("created_at DESC").as_json(:only => [:id, :report, :created_at], :include => {:location => {:only => [:address]}, :breeding_site => {:only => [:id, :description_in_es, :description_in_pt]}})}, :status => 200 and return
   end
 
   #----------------------------------------------------------------------------
