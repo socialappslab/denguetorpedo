9251ccc2727e69d1a5e9e71d359e874837809f2a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-12-08 22:49:45 -0800

    Handle Base 64 encoded images in Api::ReportsController#create.

--- app/controllers/api/v0/reports_controller.rb
+++ app/controllers/api/v0/reports_controller.rb
@@ -10,18 +10,24 @@ class API::V0::ReportsController < API::V0::BaseController
   # POST /api/v0/reports
 
   def create
-    puts "params: #{params}"
+    paperclip_image = nil
+    if params[:report][:before_photo]
+      base64_image = params[:report][:before_photo]
+      params[:report].except!(:before_photo)
+      filename             = @user.display_name.underscore + "_report.jpg"
+      paperclip_image      = prepare_base64_image_for_paperclip(base64_image, filename)
+    end
+
     @report                 = Report.new(params[:report])
     @report.reporter_id     = @user.id
     @report.neighborhood_id = @user.neighborhood_id
     @report.completed_at    = Time.now
+    @report.before_photo    = paperclip_image
 
     if params[:report][:address].present?
       location = Location.find_or_create_by_address(params[:report][:address])
       @report.location_id = location.id
     end
-
-    puts "@report: #{@report.inspect} | @report.save: #{@report.save!}"
 
     if @report.save
       render :json => @report, :status => 200 and return
