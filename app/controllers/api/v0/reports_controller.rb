6e906d3b6279acad27e892a7d89e434fc3916669
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-12-08 21:43:46 -0800

    Add create report API endpoint.
    NOTE: The test for this are failing because I haven't figured out how to
    mock uploading an image file for now.

--- app/controllers/api/v0/reports_controller.rb
+++ app/controllers/api/v0/reports_controller.rb
@@ -7,4 +7,29 @@ class API::V0::ReportsController < API::V0::BaseController
   end
 
   #----------------------------------------------------------------------------
+  # POST /api/v0/reports
+
+  def create
+    puts "params: #{params}"
+    @report                 = Report.new(params[:report])
+    @report.reporter_id     = @user.id
+    @report.neighborhood_id = @user.neighborhood_id
+    @report.completed_at    = Time.now
+
+    if params[:report][:address].present?
+      location = Location.find_or_create_by_address(params[:report][:address])
+      @report.location_id = location.id
+    end
+
+    puts "@report: #{@report.inspect} | @report.save: #{@report.save!}"
+
+    if @report.save
+      render :json => @report, :status => 200 and return
+    else
+      raise API::V0::Error.new(@report.errors.full_messages[0], 403)
+    end
+  end
+
+  #----------------------------------------------------------------------------
+
 end
--- spec/controllers/api/v0/reports_controller_spec.rb
+++ spec/controllers/api/v0/reports_controller_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe API::V0::ReportsController do
   let!(:user) { FactoryGirl.create(:user, :neighborhood_id => Neighborhood.first.id) }
   let!(:device_session) { FactoryGirl.create(:device_session, :user_id => user.id, :token => SecureRandom.uuid)}
+  let(:photo_file) 			{ File.open("spec/support/foco_marcado.jpg") }
+  let(:uploaded_photo)   { ActionDispatch::Http::UploadedFile.new(:tempfile => photo_file, :filename => File.basename(photo_file), :content_type => 'image/jpg') }
 
   it "returns status 401 if the device token is not present" do
     get :index
@@ -13,6 +15,18 @@ describe API::V0::ReportsController do
   it "returns list of user's reports" do
     request.env["DengueChat-API-V0-Device-Session-Token"] = device_session.token
     get :index, nil
-    expect(JSON.parse(response.body)).to eq([])
+    expect(JSON.parse(response.body)).to eq("reports" => [])
+  end
+
+  context "when creating a report" do
+    before(:each) do
+      request.env["DengueChat-API-V0-Device-Session-Token"] = device_session.token
+    end
+
+    it "increments Report count on successful attempt" do
+      expect {
+        post :create, :report => {:report => "Hello", :breeding_site_id => BreedingSite.first.id, :address => "N0032", :before_photo => uploaded_photo}
+      }.to change(Report, :count).by(1)
+    end
   end
 end
