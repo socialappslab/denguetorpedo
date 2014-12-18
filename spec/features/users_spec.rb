4cd12ecf0a727dcf6bd6da4bc1608c24181cec70
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-12-17 23:30:46 -0800

    Add RSpec tests.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,7 +3,8 @@
 require 'spec_helper'
 describe "Users", :type => :feature do
   context "when editing one's information" do
-    let!(:user) { FactoryGirl.create(:user, :phone_number => nil, :carrier => nil, :prepaid => nil, :neighborhood_id => Neighborhood.first.id) }
+    let!(:user)       { FactoryGirl.create(:user, :phone_number => nil, :carrier => nil, :prepaid => nil, :neighborhood_id => Neighborhood.first.id) }
+    let(:coordinator) { FactoryGirl.create(:coordinator, :role => User::Types::COORDINATOR, :neighborhood_id => Neighborhood.first.id)}
 
     before(:each) do
       sign_in(user)
@@ -37,6 +38,29 @@ describe "Users", :type => :feature do
         fill_in :user_last_name, :with => "I AM TESTER"
         click_button I18n.t("views.buttons.update")
         expect(find_field("user_last_name").value).to eq("I AM TESTER")
+      end
+    end
+
+    context "when the coordinator edits a user" do
+      before(:each) do
+        sign_out(user)
+        sign_in(coordinator)
+        visit edit_user_path(user)
+      end
+
+      it "allows to change the password" do
+        fill_in :user_password, :with => "testing"
+        fill_in :user_password_confirmation, :with => "testing"
+        click_button I18n.t("views.buttons.update")
+
+        sign_out(coordinator)
+
+        # Now, let's verify the password is indeed "testing"
+        visit root_path
+        fill_in "username", :with => user.username
+        fill_in "password", :with => "testing"
+        click_button "Entrar"
+        expect(page).to have_content( I18n.t("activerecord.models.team", :count => 3) )
       end
     end
   end
