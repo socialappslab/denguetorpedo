61165f404dc940b793066f8e0e68407e4bb4876a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-12-19 14:05:31 -0800

    Precompile assets.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,7 +46,7 @@ config.assets.js_compressor  = :yui
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( googlemap/* map.js bootstrap/typeahead.js image-compression.js)
+  config.assets.precompile += %w( googlemap/* map.js bootstrap/* image-compression.js moment.js datepicker.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
