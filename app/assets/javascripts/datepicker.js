8f16086272dcea5a9ac905a7c97a9e31de5f7cdc
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-01-27 12:16:14 -0800

    Add hours to datetimepicker.

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,7 +1,17 @@
 $(function () {
   $('.datetimepicker').datetimepicker({
     language: "#{I18n.locale.to_s}",
-    pickTime: false,
-    useCurrent: true
+    pickTime: true,
+    useCurrent: true,
+    icons: {
+      time: 'fa fa-time',
+      date: 'fa fa-calendar',
+      up: 'fa fa-chevron-up',
+      down: 'fa fa-chevron-down',
+      previous: 'fa fa-chevron-left',
+      next: 'fa fa-chevron-right',
+      today: 'fa fa-screenshot',
+      clear: 'fa fa-trash'
+    }
   });
 });
