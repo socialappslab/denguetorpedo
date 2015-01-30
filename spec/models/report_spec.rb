af22db6c6922c0c770d9f6dadb2e8b5fdd43a227
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-01-30 15:06:42 -0800

    Add custom validator to compare eliminated_at to created_at.
    Closes #429.

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -4,8 +4,9 @@ require 'spec_helper'
 require 'rack/test'
 
 describe Report do
-	let(:user) { FactoryGirl.create(:user, :neighborhood_id => Neighborhood.first.id) }
-	let(:photo_file) 			{ File.open("spec/support/foco_marcado.jpg") }
+	let(:user) 			 { FactoryGirl.create(:user, :neighborhood_id => Neighborhood.first.id) }
+	let(:photo_file) { File.open("spec/support/foco_marcado.jpg") }
+	let(:location) 	 { FactoryGirl.create(:location, :address => "Test address")}
 
 	it "validates reporter" do
 		report = Report.create
@@ -17,11 +18,20 @@ describe Report do
 		expect { r.save! }.to change(Report, :count).by(1)
 	end
 
+	it "raises an error if elimination date is before creation date" do
+		I18n.locale = "es"
+
+		report = Report.create(:report => "Saw Report",
+		:location_id => location.id, :neighborhood_id => Neighborhood.first.id,
+		:reporter => user, :breeding_site_id => BreedingSite.first.id, :eliminated_at => Time.now - 3.minutes)
+
+		expect(report.errors.full_messages).to include("Fecha de eliminación can't be before fecha de inspección")
+	end
+
 
 	#-----------------------------------------------------------------------------
 
 	context "Setting Location Status", :after_commit => true do
-		let(:location) { FactoryGirl.create(:location, :address => "Test address")}
 		before(:each) do
 			20.times do |index|
 				report = FactoryGirl.create(:report, :report => "Saw Report ##{index}", :location_id => location.id, :neighborhood_id => Neighborhood.first.id, :reporter => user, :breeding_site_id => BreedingSite.first.id)
