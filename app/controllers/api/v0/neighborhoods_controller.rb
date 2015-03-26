bfa37093f3345b2613122eae224648dde75c012b
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-03-26 07:23:10 -0700

    Calculate the chart data in API::v0 using query parameters when available.

--- app/controllers/api/v0/neighborhoods_controller.rb
+++ app/controllers/api/v0/neighborhoods_controller.rb
@@ -9,18 +9,29 @@ class API::V0::NeighborhoodsController < API::V0::BaseController
     @reports      = @neighborhood.reports
     @visit_ids    = @reports.joins(:location).pluck("locations.id")
 
-    start_time  = nil
-    visit_types = []
-    if cookies[:chart].present?
+    # Use query parameters if available, but fallback to cookies if necessary.
+    if params[:chart].present?
+      start_time = nil
+      start_time = 1.month.ago  if params[:chart][:timeframe] == "1"
+      start_time = 3.months.ago if params[:chart][:timeframe] == "3"
+      start_time = 6.months.ago if params[:chart][:timeframe] == "6"
+
+      if params[:chart][:percentages] == "cumulative"
+        @statistics = Visit.calculate_cumulative_time_series_for_locations_and_start_time(@visit_ids, start_time)
+      else
+        @statistics = Visit.calculate_daily_time_series_for_locations_start_time_and_visit_types(@visit_ids, start_time, [])
+      end
+    elsif cookies[:chart].present?
       chart_settings = JSON.parse(cookies[:chart])
+
+      start_time = nil
       start_time = 1.month.ago  if chart_settings["timeframe"] == "1"
       start_time = 3.months.ago if chart_settings["timeframe"] == "3"
       start_time = 6.months.ago if chart_settings["timeframe"] == "6"
+
+      @statistics = Visit.calculate_daily_time_series_for_locations_start_time_and_visit_types(@visit_ids, start_time, [])
     end
 
-    # TODO: We're overriding whatever options the user has chosen to
-    # keep things agile for the time being.
-    @statistics = Visit.calculate_daily_time_series_for_locations_start_time_and_visit_types(@visit_ids, start_time, [])
 
     # Format the data in a way that Google Charts can use.
     @chart_statistics = [[I18n.t('views.statistics.chart.time'), I18n.t('views.statistics.chart.percent_of_positive_sites'), I18n.t('views.statistics.chart.percent_of_potential_sites'), I18n.t('views.statistics.chart.percent_of_negative_sites')]]
