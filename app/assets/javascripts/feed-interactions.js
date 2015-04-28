ea8b206f528c6927b56096f762d1241bc8462136
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-04-27 23:18:17 -0700

    Fix commenting bug.

--- app/assets/javascripts/feed-interactions.js
+++ app/assets/javascripts/feed-interactions.js
@@ -5,8 +5,8 @@ $(document).ready(function()
   $(".comment-button").on("click", function(eventObj)
   {
     eventObj.preventDefault();
-    parent = $(eventObj.currentTarget).parents(".feed-item");
-    parent.siblings(".feed-item-new-comment").first().toggle()
+    parent = $(eventObj.currentTarget).parents(".feed-item-complete");
+    parent.children(".feed-item-new-comment").first().toggle()
   });
 
   //---------------------------------------------------------------------------
