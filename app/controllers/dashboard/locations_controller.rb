2597daa89eefce05b6ec699fc78a437726f8290a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-05-10 14:08:17 -0700

    Optimize dashboard/locations#index.

--- app/controllers/dashboard/locations_controller.rb
+++ app/controllers/dashboard/locations_controller.rb
@@ -7,7 +7,7 @@ class Dashboard::LocationsController < Dashboard::BaseController
   # GET /dashboard/locations
 
   def index
-    @locations = @neighborhood.locations
+    @locations = @neighborhood.locations.order("address ASC")
 
     @neighborhoods_select = Neighborhood.order("name ASC").map {|n| [n.name, n.id]}
   end
