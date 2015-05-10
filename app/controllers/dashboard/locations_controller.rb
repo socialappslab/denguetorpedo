39f7cf8cea8202e02c5857c93c6e3724f1ccab85
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-05-10 14:22:42 -0700

    Add pagination to reduce load time to dashboard views.

--- app/controllers/dashboard/locations_controller.rb
+++ app/controllers/dashboard/locations_controller.rb
@@ -9,6 +9,12 @@ class Dashboard::LocationsController < Dashboard::BaseController
   def index
     @locations = @neighborhood.locations.order("address ASC")
 
+    # At this point, we can start limiting the number of reports we return.
+    @pagination_count  = @locations.count
+    @pagination_limit  = 50
+    offset = (params[:page] || 0).to_i * @pagination_limit
+    @locations = @locations.limit(@pagination_limit).offset(offset)
+
     @neighborhoods_select = Neighborhood.order("name ASC").map {|n| [n.name, n.id]}
   end
 
