4fe79a00ac08109797d81d684422fcbecc7de53e
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-05-21 07:50:51 -0700

    Migrate to add likeable ability to comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,7 @@ class Comment < ActiveRecord::Base
 
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
+  has_many :likes, :as => :likeable
 
   #----------------------------------------------------------------------------
 
