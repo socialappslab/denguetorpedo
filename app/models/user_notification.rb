8b12246b03f6ade13f9dd4f6e5f615226f80156e
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-05-28 22:04:43 -0700

    Add validations and update constants in UserNotification model.

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -1,6 +1,19 @@
 # -*- encoding : utf-8 -*-
 class UserNotification < ActiveRecord::Base
-  attr_accessible :user_id, :notification_type, :viewed
+  attr_accessible :user_id, :notification_id, :notification_type, :seen_at, :notified_at
+
+  #----------------------------------------------------------------------------
+
+  module Types
+    MESSAGE = 0
+    POST    = 1
+    COMMENT = 2
+  end
+
+  module Mediums
+    WEB   = 0
+    EMAIL = 1
+  end
 
   #----------------------------------------------------------------------------
 
@@ -8,12 +21,11 @@ class UserNotification < ActiveRecord::Base
 
   #----------------------------------------------------------------------------
 
-  module Types
-    MESSAGE = 0
-  end
+  validates :user_id,           :presence => true
+  validates :notification_id,   :presence => true
+  validates :notification_type, :presence => true
+  validates :medium,            :presence => true
+  validates :notified_at,       :presence => true
 
   #----------------------------------------------------------------------------
-
-  validates :user_id,           :presence => true
-  validates :notification_type, :presence => true
 end
