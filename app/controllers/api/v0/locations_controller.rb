ca918c1902184def7488392322948c3116731e97
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-06-18 19:00:31 -0700

    Add option to load only CSV data.

--- app/controllers/api/v0/locations_controller.rb
+++ app/controllers/api/v0/locations_controller.rb
@@ -6,9 +6,18 @@ class API::V0::LocationsController < API::V0::BaseController
   # GET /api/v0/locations
   # Parameters:
   # * neighborhood_id
+  # * CSV data
   def index
     @neighborhood = Neighborhood.find_by_id(params[:neighborhood_id])
     @locations    = @neighborhood.locations.includes(:visits)
+
+    if params[:csv_only].present?
+      @locations = @neighborhood.locations.where("locations.id IN (SELECT location_id FROM csv_reports)").includes(:visits)
+    else
+      @locations = @neighborhood.locations.includes(:visits)
+    end
+
+
     render "api/v0/locations/index" and return
   end
 end
