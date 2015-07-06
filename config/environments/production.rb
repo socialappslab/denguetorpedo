94c98b0cef3f52c346802a278a85f93a0f7459fa
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-07-05 21:02:54 -0700

    Precompile google/marker-clusterer.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Dengue::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery/* googlemap/* bootstrap/* map.js csv-ajax.js image-compression.js moment.js datepicker.js)
+  config.assets.precompile += %w( jquery/* google/* googlemap/* bootstrap/* map.js csv-ajax.js image-compression.js moment.js datepicker.js)
   config.assets.precompile += %w( *.png *.jpg )
   config.assets.precompile += %w( app/* dashboard.css bootstrap/*.css jquery/*.css graphs.css )
 
