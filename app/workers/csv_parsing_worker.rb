a92d81f209f8e4c2873b4d161eaa54f6458c0b15
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-07-12 13:26:03 -0700

    Deprecate MapCoordinatesWorker. Add CsvParsingWorker template.

---
+++ app/workers/csv_parsing_worker.rb
@@ -0,0 +1,11 @@
+-*- encoding : utf-8 -*-
+require "sidekiq"
+
+class CsvParsingWorker
+  include Sidekiq::Worker
+
+  sidekiq_options :queue => :csv_parsing, :retry => true, :backtrace => true
+
+  def perform
+  end
+end
