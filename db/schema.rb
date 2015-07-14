8bbb114ceb0547b634dd661ea72a7ab6e3f19d84
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-07-13 17:01:58 -0700

    Migrate to add csv_errors table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150712230506) do
+ActiveRecord::Schema.define(:version => 20150713235458) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -134,6 +134,13 @@ ActiveRecord::Schema.define(:version => 20150712230506) do
 
   create_table "countries", :force => true do |t|
     t.string "name"
+  end
+
+  create_table "csv_errors", :force => true do |t|
+    t.integer  "csv_report_id"
+    t.integer  "error_type"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "csv_reports", :force => true do |t|
