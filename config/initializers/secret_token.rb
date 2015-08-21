557fd5c9f71d7620ed101449709b5cd9cbce1956
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-08-20 18:37:46 -0700

    Add secret_key_base.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dengue::Application.config.secret_token = 'bcde7886d24763531679077b47da15523df11f8f43933afda9d544436439915bbaba085b3ae21dc69ed83231b9e0825c20f7cfbf5f4ebe7d0b27c571312a4660'
+Dengue::Application.config.secret_token    = 'bcde7886d24763531679077b47da15523df11f8f43933afda9d544436439915bbaba085b3ae21dc69ed83231b9e0825c20f7cfbf5f4ebe7d0b27c571312a4660'
+
+# TODO: Uncomment this once you've completely migrated to Rails 4.0.
+# Dengue::Application.config.secret_key_base = 't4uvHf4uvHfUPvHfqf1cbcwgCcKWjHqcHDa6DNWGHnZq4CtDa6DNWGwgCcbfDEWZWjkpUPfuGHEcqcMFjrkfuGKWZcEt4unkpUPMFjkpDprfUPfuGuGMFjrKWjHnZq4C'
