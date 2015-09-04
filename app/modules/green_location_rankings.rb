251ac1d8724c04eceabec232d6f23a10eca09a38
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-09-03 18:01:25 -0700

    Add GreenLocationRankings module to interact with Redis.

---
+++ app/modules/green_location_rankings.rb
@@ -0,0 +1,22 @@
+# This module abstracts the notion of a green locations rankings table by
+# using Redis in-memory datastore to store rankings. Use this method
+# to retrieve top 5 results, and use this method to periodically store
+# new rankings (run via a Sidekiq job).
+
+module GreenLocationRankings
+  def self.add_score_to_user(score, user)
+    $redis_pool.with do |redis|
+      redis.zadd(self.redis_key, score, user.id)
+    end
+  end
+
+  def self.top_ten
+    $redis_pool.with do |redis|
+      redis.zrevrange(self.redis_key, 0, 10, :with_scores => true).map {|id, score| {:user => User.find(id), :score => score} }
+    end
+  end
+
+  def self.redis_key
+    "green_location_rankings"
+  end
+end
