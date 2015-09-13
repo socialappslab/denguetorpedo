187e6ee5741aa48dcd95b8d7bda3f7244ceb0cb7
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-09-12 17:16:40 -0700

    Add button, AngularJS controller and Rails action to destroy a Post.

--- app/controllers/api/v0/posts_controller.rb
+++ app/controllers/api/v0/posts_controller.rb
@@ -84,12 +84,17 @@ class API::V0::PostsController < API::V0::BaseController
   # DELETE /api/v0/posts/:id
 
   def destroy
-    @post = @current_user.posts.find( params[:id] )
+    if @current_user.coordinator?
+      @post = Post.find( params[:id] )
+    else
+      @post = @current_user.posts.find( params[:id] )
+    end
 
+    user = @post.user
     if @post.destroy
-      points = @current_user.total_points || 0
-      @current_user.update_column(:total_points, points - User::Points::POST_CREATED)
-      @current_user.teams.each do |team|
+      points = user.total_points || 0
+      user.update_column(:total_points, points - User::Points::POST_CREATED)
+      user.teams.each do |team|
         team.update_column(:points, team.points - User::Points::POST_CREATED)
       end
 
