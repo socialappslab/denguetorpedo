29d27f9da8e33e03ff42bca90cb936d914a5b0a2
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-09-24 15:30:29 -0700

    Display only small green locations chart.

--- app/assets/javascripts/app/controllers/green_locations_chart_controller.js
+++ app/assets/javascripts/app/controllers/green_locations_chart_controller.js
@@ -2,7 +2,7 @@ var chartCtrl = function ($scope, $http, $cookies, $attrs) {
   $scope.chartLoading = false;
   $scope.noChartData  = false;
 
-  $scope.refreshChartWithParams = function() {
+  $scope.refreshChart = function(chartID) {
     $scope.chartLoading = true;
 
     var ajax = $http({url: $attrs.greenLocationsPath, method: "GET"});
@@ -12,8 +12,7 @@ var chartCtrl = function ($scope, $http, $cookies, $attrs) {
         $scope.noChartData = true;
       } else {
         $scope.noChartData = false;
-        drawChart("green-locations-chart-minor", response.green_locations);
-        drawChart("green-locations-chart-major", response.green_locations);
+        drawChart(chartID, response.green_locations);
       }
     });
     ajax.error(function(response) {
@@ -24,11 +23,12 @@ var chartCtrl = function ($scope, $http, $cookies, $attrs) {
     });
   }
 
-  $scope.refreshChartWithParams();
+  $scope.refreshChart("green-locations-chart-minor");
 
-  $scope.showGreenLocationsChartModal = function() {
-    $('#green-locations-chart-modal').modal('show');
-  }
+  // $scope.showGreenLocationsChartModal = function() {
+  //   $('#green-locations-chart-modal').modal('show');
+  //   $scope.refreshChart("green-locations-chart-major");
+  // }
 };
 
 
@@ -39,8 +39,6 @@ var chartCtrl = function ($scope, $http, $cookies, $attrs) {
 angular.module('dengueChatApp').controller("greenLocationsChartCtrl", ["$scope", "$http", "$cookies", "$attrs", chartCtrl]);
 
 
-
-
 function drawChart(chartID, data) {
   var dataTable = new google.visualization.DataTable();
   var dateColumn = dataTable.addColumn("string", "Semana")
@@ -49,36 +47,36 @@ function drawChart(chartID, data) {
   dataTable.addColumn({type: 'number', role: 'annotation'});
 
   for (var i = 0; i < data.length; i++) {
-    var tooltipText = data[i].week + ": " + data[i].count;
+    var tooltipText = data[i].week + " tenido " + data[i].count + " casas verdes";
     dataTable.addRow([data[i].week, data[i].count, tooltipText, data[i].count])
   }
 
   var element = document.getElementById(chartID);
   var options = {
+    width: element.offsetWidth,
     chartArea: {
-      left: 50,
-      right: 50,
-      top: 0,
-      bottom: 0,
       width: "100%",
-      height: "90%"
+      height: "80%",
+      left: 25
     },
     colors: ["#5cb85c"],
     hAxis: {
+      title: "Semana",
       gridlines: {
         color: "transparent"
       }
     },
     vAxis: {
-      baselineColor: '#fff',
-      gridlineColor: '#fff',
-      textPosition: 'none',
-      minValue: 0,
-      gridlines: {
-        color: "transparent"
-      }
-    }
+      title: "Casas Verdes",
+      textPosition: 'in',
+      minValue: 0
+    },
+    legend: "none"
   };
+
+  if (chartID == "green-locations-chart-minor")
+    options.hAxis.textPosition = 'none';
+
 
   var chart = new google.visualization.ColumnChart(document.getElementById(chartID));
   var view = new google.visualization.DataView(dataTable);
