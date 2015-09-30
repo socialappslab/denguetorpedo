63efd9d6e521c7071e0b2ba447239cbeaf9c8926
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-09-30 11:13:56 -0700

    Convert to using Dependency Injection in AngularJS.

---
+++ app/assets/javascripts/app/directives/remote-submit.js
@@ -0,0 +1,44 @@
+
+var directive = function($http){
+  return {
+    restrict: "A",
+    link: function(scope, element, attrs) {
+      element.on("submit", function(event) {
+        event.preventDefault();
+
+        element.find(".fa-spin").show();
+        element.find(":submit").attr("disabled", true)
+
+        var fd   = new FormData();
+        var form = element.serializeArray();
+        for (var i = 0; i < form.length; i++) {
+          fd.append( form[i].name, form[i].value );
+        }
+
+        $.ajax({
+          url:  event.currentTarget.action,
+          type: event.currentTarget.method,
+          data: fd,
+          contentType: false,
+          processData: false,
+          success: function(response) {
+            window.alert("Éxito!");
+            window.location.href = response.redirect_path;
+          },
+
+          error: function(response) {
+            alert(response.responseJSON.message);
+          },
+
+          complete: function(response) {
+            element.find(".fa-spin").hide();
+            element.find(":submit").attr("disabled", false)
+
+          }
+        })
+      })
+    }
+  }
+}
+
+angular.module("denguechat.directives").directive("remoteSubmit", ["$http", directive]);
