7ec66a76f378c61d765be1bd4a72b2918fb357ff
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-10-02 16:47:19 -0700

    Remove deprecated timeout.rb

--- config/initializers/timeout.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-# Use Rack::Timeout to time out Puma workers. See
-# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-# for more.
-# Rack::Timeout.timeout = 20 # seconds
