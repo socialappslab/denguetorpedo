58d871cb54c2acce2fff7b806c2cfcae335a80a4
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-10-13 15:11:02 -0700

    Compare previous score and generate Post instance if user detected green houses.

--- app/workers/green_location_rankings_worker.rb
+++ app/workers/green_location_rankings_worker.rb
@@ -11,7 +11,31 @@ class GreenLocationRankingsWorker
 
   def perform
     User.find_each do |u|
-      GreenLocationRankings.add_score_to_user(u.green_locations.count, u)
+      current_score = GreenLocationRankings.score_for_user(u)
+      new_score     = u.green_locations.count
+
+      # Generate a new post congratulating the user if their new score is higher
+      # than last score.
+      if current_score && new_score && new_score > current_score
+        differential = (new_score - current_score).to_i
+        points       = differential * User::Points::GREEN_HOUSE
+
+        content = "¡Felicitaciones! <a href='#{Rails.application.routes.url_helpers.user_path(u)}'>@#{u.username}</a>,"
+        content += "lograste #{differential} casas verdes y ganaste #{points} puntos!"
+
+        post                 = Post.new
+        post.content         = content
+        post.user_id         = u.id
+        post.neighborhood_id = u.neighborhood_id
+        post.save!
+
+        # Create notification for that particular user.
+        Time.use_zone("America/Guatemala") do
+          UserNotification.create(:user_id => u.id, :notification_id => post.id, :notification_type => "Post", :notified_at => Time.zone.now, :medium => UserNotification::Mediums::WEB)
+        end
+      end
+
+      GreenLocationRankings.add_score_to_user(new_score, u)
     end
 
     GreenLocationRankingsWorker.perform_in(1.day)
