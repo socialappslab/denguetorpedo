a4bc5f6ee8149e4e9251f4faa72e77ebc417e237
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-28 18:55:07 -0700

    Remove tables that don't exist in Production.

--- db/migrate/20160328233314_remove_location_statuses_houses.rb
+++ db/migrate/20160328233314_remove_location_statuses_houses.rb
@@ -1,9 +1,7 @@
 class RemoveLocationStatusesHouses < ActiveRecord::Migration
   def change
-    drop_table :location_statuses
     drop_table :badges
     drop_table :buy_ins
-    drop_table :ckeditor_assets
     drop_table :contacts
     drop_table :feedbacks
     drop_table :feeds
