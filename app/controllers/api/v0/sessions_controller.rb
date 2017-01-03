a3d3077cd6715ac60dd536497d31f10612c0aa1e
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-01-03 13:46:58 -0800

    Returns user-specific information for the user.

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -11,7 +11,20 @@ class API::V0::SessionsController < API::V0::BaseController
     user = User.find_by_email( params[:username] ) if user.nil?
 
     if user.present? && user.authenticate(params[:password])
-      render :json => { :token => user.jwt_token }, :status => 200
+      render :json => {
+        :token => user.jwt_token,
+        :user => {
+          :display_name  => user.display_name,
+          :profile_photo => user.profile_photo.url(:thumbnail),
+          :neighborhood  => {
+            :id   => user.neighborhood_id,
+            :name => user.neighborhood.geographical_display_name
+          },
+          :neighborhoods => Neighborhood.all.as_json(:only => [:id, :name]),
+          :total_points  => user.total_total_points,
+          :green_locations => GreenLocationRankings.score_for_user(user).to_i
+        }
+      }, :status => 200
     else
       raise API::V0::Error.new("Invalid email or password. Please try again.", 401) and return
     end
