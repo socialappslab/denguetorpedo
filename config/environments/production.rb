a6e04475663caa63a2b2ef2b5ce2683a0c8485f3
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-07-24 01:07:30 -0700

    Adds select css.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Dengue::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w(google/marker-clusterer.js csv-ajax.js datepicker.js google-maps.js)
   config.assets.precompile += %w(bootstrap/typeahead.js bootstrap/bootstrap-multiselect.css bootstrap/marketing.css)
-  config.assets.precompile += %w(dashboard.css graphs.css)
+  config.assets.precompile += %w(dashboard.css graphs.css select.css select-customizations.css)
   config.assets.precompile += %w( *.png *.jpg )
   config.assets.paths << Rails.root.join("app", "assets", "templates")
 
