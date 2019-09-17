b023f01390cdced48e52153825600a7a6398c482
denisde21 <ddbenitez21@gmail.com>
2019-09-17 18:49:01 -0400

    facebook compartir

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,5 +7,8 @@
 # no regular words or you'll be exposed to dictionary attacks.
 Dengue::Application.config.secret_token    = 'bcde7886d24763531679077b47da15523df11f8f43933afda9d544436439915bbaba085b3ae21dc69ed83231b9e0825c20f7cfbf5f4ebe7d0b27c571312a4660'
 
+#Dengue::Application.config.secret_token    = '2e8aa86d182203c468a9d4f0109ef701'
+
+
 # TODO: Uncomment this once you've completely migrated to Rails 4.0.
 # Dengue::Application.config.secret_key_base = 't4uvHf4uvHfUPvHfqf1cbcwgCcKWjHqcHDa6DNWGHnZq4CtDa6DNWGwgCcbfDEWZWjkpUPfuGHEcqcMFjrkfuGKWZcEt4unkpUPMFjkpDprfUPfuGuGMFjrKWjHnZq4C'
