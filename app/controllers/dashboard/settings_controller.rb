a6b1a32b89366312e553de00378386483133ee80
sofiarivas93 <sofiarivas1993@gmail.com>
2021-04-18 23:23:57 -0400

    Update settings

--- app/controllers/dashboard/settings_controller.rb
+++ app/controllers/dashboard/settings_controller.rb
@@ -10,40 +10,73 @@ class Dashboard::SettingsController < Dashboard::BaseController
     @city = current_user.city
   end
   def create
-    Rails.logger.info("PRUEBA 1-------------------||||")
-    Rails.logger.info(params[:organization_id])
-    if params[:organization_id].blank?
-      Rails.logger.info("PRUEBA 2-------------------||||")
-      flash[:alert] = "Debe seleccionar una organización"
-      redirect_to dashboard_settings_path and return
+    Rails.logger.info("PRUEBA 2-------------------||||")
+    Rails.logger.info(params[:organizations_select])
 
+    if params[:organizations_select].blank?
+      redirect_to dashboard_settings_path, :flash => { :alert => "Debe seleccionar una organización" } and return
     end
+
+    if !params[:dataVisits].blank?
+      @visits = Parameter.where(:organization_id => params[:organizations_select], :key => 'organization.data.visits.url')
+      Rails.logger.info("visita------------------||||")
+      Rails.logger.info(!@visits.blank?)
+      if @visits.blank?
+        dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.visits.url' , :value => params[:dataVisits])
+        dataVisits.save
+      else
+        dataVisits = @visits.update_column(:value, params[:dataVisits])
+        dataVisits.save
+      end
+    end
+
 =begin
-    Rails.logger.info(params[:volunteers])
-    dataVisits = Parameter.new(:organization_id => params[:organization_id], :key => 'organization.data.visits.url' , :value => params[:dataVisits])
-    dataLocations = Parameter.new(:organization_id => params[:organization_id], :key => 'organization.data.locations.url' , :value => params[:dataLocations])
-    datainspections = Parameter.new(:organization_id => params[:organization_id], :key => 'organization.data.inspections.url' , :value => params[:datainspections])
+    @parameter_value = Parameter.where(:organization_id => params[:organizations_select])
+    @parameter_value.each do |row|
+      if value_exist(row,'organization.data.visits.url' )
+        Rails.logger.info("ya merito")
+        Rails.logger.info(row)
+        dataVisits = row.update_column(:value, params[:dataVisits])
+        Rails.logger.info(dataVisits)
+      else
+        Rails.logger.info("va ser create")
+        Rails.logger.info(row)
+        dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.visits.url' , :value => params[:dataVisits])
+        dataVisits.save
+      end
+    end
+=end
+
+=begin
+    dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.visits.url' , :value => params[:dataVisits])
+    dataLocations = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.locations.url' , :value => params[:dataLocations])
+    datainspections = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.inspections.url' , :value => params[:datainspections])
     @user_params = User.find_by_id(params[:volunteers])
     Rails.logger.info(@user_params.username)
-    volunteers = Parameter.new(:organization_id => params[:organization_id], :key => 'organization.sync.default-user' , :value => @user_params.username)
+    volunteers = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.sync.default-user' , :value => @user_params.username)
 
     dataVisits.save
     dataLocations.save
     datainspections.save
     volunteers.save
 
-
     if dataVisits.save && dataLocations.save && datainspections.save && volunteers.save
       redirect_to dashboard_settings_path, :flash => { :notice => I18n.t("views.coordinators.users.success_create_flash") } and return
     else
-      render "coordinator/users/new", flash: { alert: @user.errors.full_messages.join(', ') }
+      render dashboard_settings_path, flash: { alert: "ERROR" }
     end
 =end
+  end
 
+  def value_exist(parameter,value)
+    Rails.logger.info(parameter)
+      if parameter.key == value
+        return true
+      end
+    return false
   end
 
   def organizations_select
-    Rails.logger.info("organizations_select-------------------")
     @select = Parameter.where(:organization_id => params[:id])
     render json: @select.to_json, status:200
 
