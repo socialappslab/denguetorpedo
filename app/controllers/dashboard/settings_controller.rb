9290021da1329a60e3a55ffbfebbd7f93dc7544b
denisde21 <ddbenitez21@gmail.com>
2021-04-19 22:04:02 -0400

    fix settings

--- app/controllers/dashboard/settings_controller.rb
+++ app/controllers/dashboard/settings_controller.rb
@@ -19,16 +19,46 @@ class Dashboard::SettingsController < Dashboard::BaseController
 
     if !params[:dataVisits].blank?
       @visits = Parameter.where(:organization_id => params[:organizations_select], :key => 'organization.data.visits.url')
-      Rails.logger.info("visita------------------||||")
-      Rails.logger.info(!@visits.blank?)
       if @visits.blank?
         dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.visits.url' , :value => params[:dataVisits])
         dataVisits.save
       else
-        dataVisits = @visits.update_column(:value, params[:dataVisits])
-        dataVisits.save
+        @visits.first.update_column(:value, params[:dataVisits])
       end
     end
+
+    if !params[:dataLocations].blank?
+      @visits = Parameter.where(:organization_id => params[:organizations_select], :key => 'organization.data.locations.url')
+      if @visits.blank?
+        dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.locations.url' , :value => params[:dataLocations])
+        dataVisits.save
+      else
+        @visits.first.update_column(:value, params[:dataLocations])
+      end
+    end
+
+    if !params[:datainspections].blank?
+      @visits = Parameter.where(:organization_id => params[:organizations_select], :key => 'organization.data.inspections.url')
+      if @visits.blank?
+        dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.data.inspections.url' , :value => params[:datainspections])
+        dataVisits.save
+      else
+        @visits.first.update_column(:value, params[:datainspections])
+      end
+    end
+
+    if !params[:organizations_select].blank?
+      @visits = Parameter.where(:organization_id => params[:organizations_select], :key => 'organization.sync.default-user')
+      @user_params = User.find_by_id(params[:volunteers])
+      if @visits.blank?
+        dataVisits = Parameter.new(:organization_id => params[:organizations_select], :key => 'organization.sync.default-user' , :value => @user_params.username)
+        dataVisits.save
+      else
+        @visits.first.update_column(:value, @user_params.username)
+      end
+    end
+
+    redirect_to dashboard_settings_path, :flash => { :notice => I18n.t("views.coordinators.users.success_create_flash") } and return
 
 =begin
     @parameter_value = Parameter.where(:organization_id => params[:organizations_select])
@@ -83,7 +113,7 @@ class Dashboard::SettingsController < Dashboard::BaseController
   end
   def users_select
     Rails.logger.info("users_select-------------------")
-    @select = User.select("id", "name").where(:username => params[:username])
+    @select = User.select("id", "first_name", "last_name", "name").where(:username => params[:username])
     render json: @select.to_json, status:200
 
   end
